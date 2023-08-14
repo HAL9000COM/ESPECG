b2f95a9c3d1d08f5b33c32ae97c22377a77ac656
HAL9000COM <f1226942353@icloud.com>
2023-08-14 11:06:17 +0800

    Use Ray Multiprocessing.

--- data_analysis/tf_dataprep.py
+++ data_analysis/tf_dataprep.py
@@ -1,13 +1,13 @@
 # %%
 import numpy as np
-import pandas as pd
 import os
 import tensorflow as tf
+from ray.util.multiprocessing import Pool
 
 # %%
 # Define the path to the parent directory containing the CSV files
-DATA_TYPE = "beat"
-SAMPLE_LENGTH = 360
+DATA_TYPE = "rhythm"
+SAMPLE_LENGTH = 3600
 
 parent_dir = "mit-bih-arrhythmia-database-1.0.0"
 tf_data_save_path = "tf_data"
@@ -20,23 +20,33 @@ elif DATA_TYPE == "rhythm":
     tf_data_save_path = os.path.join(tf_data_save_path, "rhythm_data")
 # get the list of all the CSV files in the parent directory
 dir_list = os.listdir(parent_dir)
+
+
+# %%
+def read_csv(file):
+    csv_data = np.loadtxt(file, delimiter=",", skiprows=1)
+    csv_data = np.squeeze(csv_data)
+    return csv_data
+
+
 # %%
 data_list = []
 label_list = []
+p = Pool()
 for dir in dir_list:
     file_list = os.listdir(os.path.join(parent_dir, dir))
-    for file in file_list:
-        if file.endswith(".csv"):
-            # Read the CSV file into a Pandas data frame
-            df = pd.read_csv(os.path.join(parent_dir, dir, file))
-            # Convert the Pandas data frame to a NumPy array
-            csv_data = df.to_numpy()
-            csv_data = np.squeeze(csv_data)
-
-            # Append the NumPy array to the Python list
-            data_list.append(csv_data)
-            label_list.append(dir_list.index(dir))
+    file_paths = [
+        os.path.join(parent_dir, dir, file)
+        for file in file_list
+        if file.endswith(".csv")
+    ]
+    csv_data_list = p.map(read_csv, file_paths)
+    for csv_data in csv_data_list:
+        # Append the NumPy array to the Python list
+        data_list.append(csv_data)
+        label_list.append(dir_list.index(dir))
     print(dir + " done")
+p.close()
 
 # %%
 data = tf.keras.utils.pad_sequences(
