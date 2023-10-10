3f2a97d6b29438fa28d6af45b39b489ee2ec7598
HAL9000COM <f1226942353@icloud.com>
2023-10-10 19:45:04 +0800

    Interference demo with tflite.

--- data_analysis/train.py
+++ data_analysis/train.py
@@ -24,7 +24,7 @@ print(f"The labels in the dataset are {label_list}.")
 print(f"The number of labels is {len(label_list)}.")
 # %%
 split_ratio = 0.8
-batch_size = 32
+batch_size = 128
 
 # shuffle the data
 data_train, data_test = tf.keras.utils.split_dataset(
@@ -37,13 +37,13 @@ data_train = (
     .prefetch(buffer_size=tf.data.AUTOTUNE)
 )
 data_test = data_test.cache().batch(batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
-
+dataset = dataset.cache().batch(batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
 # %%
 # Define the model
 model = tf.keras.Sequential(
     [
-        tf.keras.layers.InputLayer(input_shape=(3600,)),
-        tf.keras.layers.Reshape(target_shape=(3600, 1)),
+        tf.keras.layers.InputLayer(input_shape=(360,)),
+        tf.keras.layers.Reshape(target_shape=(360, 1)),
         tf.keras.layers.Conv1D(filters=32, kernel_size=5, strides=2, activation="relu"),
         tf.keras.layers.MaxPooling1D(pool_size=2, strides=2),
         tf.keras.layers.Conv1D(filters=64, kernel_size=5, strides=2, activation="relu"),
@@ -54,7 +54,7 @@ model = tf.keras.Sequential(
         tf.keras.layers.MaxPooling1D(pool_size=2, strides=2),
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(64, activation="relu"),
-        tf.keras.layers.Dense(16, activation="softmax"),
+        tf.keras.layers.Dense(17, activation="softmax"),
     ]
 )
 
@@ -67,9 +67,53 @@ model.compile(
 
 # Print the model summary
 model.summary()
+# %%
+# Define the callbacks
+callbacks = [
+    tf.keras.callbacks.EarlyStopping(
+        monitor="val_loss", patience=20, restore_best_weights=True
+    ),
+    tf.keras.callbacks.ReduceLROnPlateau(
+        monitor="val_loss", factor=0.1, patience=5, verbose=1
+    ),
+]
+
 
 # %%
 # Train the model
-model.fit(data_train, epochs=10, validation_data=data_test)
+model.fit(data_train, epochs=100, validation_data=data_test, callbacks=callbacks)
 
+# %%
+# draw confusion matrix with percentage
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+import pandas as pd
+import numpy as np
+
+# get confusion matrix
+y_pred = model.predict(data_train)
+y_pred = np.argmax(y_pred, axis=1)
+y_true = np.concatenate([y for x, y in data_train], axis=0)
+cm = confusion_matrix(y_true, y_pred)
+cm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
+# draw confusion matrix
+plt.figure(figsize=(10, 10))
+sns.heatmap(cm, annot=True, fmt=".2f", cmap="Blues", square=True)
+plt.ylabel("Actual label")
+plt.xlabel("Predicted label")
+plt.xticks(np.arange(17) + 0.5, label_list, rotation=90)
+plt.yticks(np.arange(17) + 0.5, label_list, rotation=0)
+plt.title("Confusion matrix")
+plt.show()
+# %%
+from tinymlgen import port
+
+c_port = port(model,pretty_print=True, optimize=False)
+print(c_port)
+# %%
+#save model
+model.save("model.h5")
+with open("model.h", "w") as f:
+    f.write(c_port)
 # %%
--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,10 +1,19 @@
 #include <SPI.h>
-
 #include <TFT_eSPI.h> // Hardware-specific library
-
 #include <esp_adc_cal.h>
 #include <SD.h>
 #include <FS.h>
+#include <EloquentTinyML.h>
+#include <eloquent_tinyml/tensorflow.h>
+
+#include "ml_model.h"
+
+#define N_INPUTS 360
+#define N_OUTPUTS 17
+// in future projects you may need to tweak this value: it's a trial and error process
+#define TENSOR_ARENA_SIZE 80 * 1024
+
+Eloquent::TinyML::TensorFlow::TensorFlow<N_INPUTS, N_OUTPUTS, TENSOR_ARENA_SIZE> tf;
 
 // Define custom pin
 const int LO_P = 34;
@@ -12,14 +21,13 @@ const int LO_N = 35;
 
 // Define ADC parameters
 const int ADC_PIN = 33;                              // Analog input pin
-const adc1_channel_t adcChannel = ADC1_CHANNEL_5;    // ADC channel
+const int ref_PIN = 32;                              // Analog input pin
+const adc1_channel_t ecgChannel = ADC1_CHANNEL_5;    // ADC channel
+const adc1_channel_t refChannel = ADC1_CHANNEL_5;    // ADC channel
 const adc_bits_width_t ADC_WIDTH = ADC_WIDTH_BIT_12; // ADC resolution
 const adc_atten_t ADC_ATTEN = ADC_ATTEN_DB_11;       // ADC attenuation
 esp_adc_cal_characteristics_t adcCal;
-
-// Define SD card parameters
-const int SD_CS = 26;
-const int SD_FREQ = 4000000;
+// #define CUSTOM_ADC // Comment this line to use the default ADC library
 
 // Define timer interrupt parameters
 const int SAMPLE_RATE = 200;                          // Sample rate in Hz (e.g., 1000 samples per second)
@@ -43,17 +51,92 @@ bool Lead_off = false;
 
 TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
 
+// Define SD card parameters
+const int SD_CS = 26;
+const int SD_FREQ = 4000000;
+File dataFile;
+
+class CircularBuffer
+{
+public:
+  CircularBuffer(int size) : size_(size), buffer_(new int[size]), head_(0), tail_(0), count_(0) {}
+
+  ~CircularBuffer()
+  {
+    delete[] buffer_;
+  }
+
+  void push(int data)
+  {
+    buffer_[head_] = data;
+    head_ = (head_ + 1) % size_;
+    if (count_ < size_)
+    {
+      count_++;
+    }
+    else
+    {
+      tail_ = (tail_ + 1) % size_;
+    }
+  }
+
+  int *read()
+  {
+    int *data = new int[count_];
+    for (int i = 0; i < count_; i++)
+    {
+      data[i] = buffer_[(tail_ + i) % size_];
+    }
+    return data;
+  }
+
+private:
+  int size_;
+  int *buffer_;
+  int head_;
+  int tail_;
+  int count_;
+};
+
+CircularBuffer predictBuffer(N_INPUTS);
+
+#ifdef CUSTOM_ADC
+#include <soc/sens_reg.h>
+#include <soc/sens_struct.h>
+int IRAM_ATTR local_adc1_read(int channel)
+{
+  uint16_t adc_value;
+  SENS.sar_meas_start1.sar1_en_pad = (1 << channel); // only one channel is selected
+  while (SENS.sar_slave_addr1.meas_status != 0)
+    ;
+  SENS.sar_meas_start1.meas1_start_sar = 0;
+  SENS.sar_meas_start1.meas1_start_sar = 1;
+  while (SENS.sar_meas_start1.meas1_done_sar == 0)
+    ;
+  adc_value = SENS.sar_meas_start1.meas1_data_sar;
+  return adc_value;
+}
+
 void IRAM_ATTR onTimer()
 {
   portENTER_CRITICAL_ISR(&timerMux);
-  int data = adc1_get_raw(adcChannel);
+  int data = local_adc1_read(ecgChannel);
   buffer[bufferIndex] = data;
   bufferIndex = (bufferIndex + 1) % BUFFER_SIZE;
   newDataAvailable = true;
   portEXIT_CRITICAL_ISR(&timerMux);
 }
-
-File dataFile;
+#else
+void IRAM_ATTR onTimer()
+{
+  portENTER_CRITICAL_ISR(&timerMux);
+  int data = adc1_get_raw(ecgChannel);
+  buffer[bufferIndex] = data;
+  bufferIndex = (bufferIndex + 1) % BUFFER_SIZE;
+  newDataAvailable = true;
+  portEXIT_CRITICAL_ISR(&timerMux);
+}
+#endif
 
 void setup()
 {
@@ -76,7 +159,7 @@ void setup()
   pinMode(0, INPUT);
   // Configure ADC
   adc1_config_width(ADC_WIDTH);
-  adc1_config_channel_atten(adcChannel, ADC_ATTEN);
+  adc1_config_channel_atten(ecgChannel, ADC_ATTEN);
 
   // Configure timer interrupt
   timer = timerBegin(0, 80, true); // Timer 0, prescaler 80
@@ -85,26 +168,35 @@ void setup()
   timerAlarmEnable(timer);
 
   // Calibrate ADC
-
   esp_adc_cal_value_t adcValue = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN, ADC_WIDTH, ESP_ADC_CAL_VAL_EFUSE_VREF, &adcCal);
+  adc1_get_raw(ecgChannel);
+  tf.begin(model_data);
+
+  // check if model loaded fine
+  if (!tf.isOk())
+  {
+    Serial.print("ERROR: ");
+    Serial.println(tf.getErrorMessage());
+  }
 }
 
 void loop()
 {
   if (digitalRead(LO_P) == HIGH)
   {
-    Serial.println("LA lead off");
+    //Serial.println("LA off");
     Lead_off = true;
   }
   else if (digitalRead(LO_N) == HIGH)
   {
-    Serial.println("RA lead off");
+   // Serial.println("RA off");
     Lead_off = true;
   }
   else
   {
     Lead_off = false;
   }
+
   if (newDataAvailable)
   {
     portENTER_CRITICAL_ISR(&timerMux);
@@ -120,17 +212,19 @@ void loop()
     {
       sample_count++;
       tft_update(localBuffer[i]);
+      predictBuffer.push(localBuffer[i]);
       if (dataFile)
       {
         sd_write(readADCValue(localBuffer[i]));
       }
     }
-
-    if (digitalRead(0) == LOW & dataFile)
-    {
-      Serial.println("Close File");
-      closeSDFile();
-    }
+    int prediction = predict();
+    Serial.println(prediction);
+  }
+  if (digitalRead(0) == LOW & dataFile)
+  {
+    Serial.println("Close File");
+    closeSDFile();
   }
 }
 
@@ -200,4 +294,28 @@ int readADCValue(uint32_t adc_raw)
 {
   uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_raw, &adcCal);
   return voltage;
+}
+
+int predict()
+{
+  float output[N_OUTPUTS];
+  int *raw_data = predictBuffer.read();
+  float input[N_INPUTS]; // change type to float
+  for (int i = 0; i < N_INPUTS; i++)
+  {
+    input[i] = static_cast<float>(raw_data[i]); // cast to float
+  }
+
+  tf.predict(input, output);
+  int max_index = 0;
+  float max_value = 0;
+  for (int i = 0; i < N_OUTPUTS; i++)
+  {
+    if (output[i] > max_value)
+    {
+      max_value = output[i];
+      max_index = i;
+    }
+  }
+  return max_index;
 }
